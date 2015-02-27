817dd2919b79d685a0c17b7cb36ad7a9a0f332dc
Sean Parsons <github@futurenotfound.com>
2015-02-27 20:38:55 +0000

    Nearly there with the circuit breaker.

--- src/ServiceFabric.hs
+++ src/ServiceFabric.hs
@@ -1,12 +1,18 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module ServiceFabric where
 
 import ServiceFabric.Types
 import Data.Cache.LRU.IO
 import qualified Data.Cache.LRU.IO as LRU
 import Control.Monad.IO.Class
+import Data.IORef
+import Data.Time.Clock.POSIX
+import Control.Monad.CatchIO
 
 {-
 - Caching.
+- Timeouts.
 - Fail fast/circuit breaker.
 - Multi get optimisations.
 - Performance stats.
@@ -16,8 +22,8 @@ import Control.Monad.IO.Class
 - Request duplication, with completion based on the first successful result.
 -}
 
-cacheWith :: (MonadIO m) => (a -> m b) -> (a -> b -> m ()) -> BasicService m a b -> BasicService m a b
-cacheWith service lookupWith insertWith = 
+cacheWith :: (MonadIO m) => (a -> m (Maybe b)) -> (a -> b -> m ()) -> BasicService m a b -> BasicService m a b
+cacheWith lookupWith insertWith service = 
   let fallback request = do
                             result <- service request
                             insertWith request result
@@ -32,3 +38,45 @@ cacheWithAtomicLRU lru service =
   let lookupWith request            = liftIO $ LRU.lookup request lru
       insertWith request response   = liftIO $ insert request response lru
   in  cacheWith lookupWith insertWith service
+
+data CircuitBreakerOptions = CircuitBreakerOptions {
+    maxFailures       :: Int
+  , resetTimeoutSecs  :: Int
+  , failureDetails    :: String
+}
+
+defaultCircuitBreakerOptions = CircuitBreakerOptions { maxFailures = 3, resetTimeoutSecs = 60, failureDetails = "Circuit breaker open."}
+
+data CircuitBreakerStatus = CircuitBreakerClosed Int | CircuitBreakerOpen Int
+
+circuitBreaker :: (MonadCatchIO m) => CircuitBreakerOptions -> BasicService m a b -> m (IORef CircuitBreakerStatus, BasicService m a b)
+circuitBreaker options service = 
+  let getCurrentTime              = round `fmap` getPOSIXTime
+      failureMax                  = maxFailures options
+      callIfClosed request ref    = do 
+                                      result <- bracketOnError (return ()) (\_ -> incErrors ref) (\_ -> service request)
+                                      liftIO $ writeIORef ref $ CircuitBreakerClosed 0
+                                      return result
+      incErrors ref               = do
+                                      currentTime <- getCurrentTime
+                                      atomicModifyIORef' ref $ \status -> case status of
+                                        (CircuitBreakerClosed errorCount) -> (if errorCount >= failureMax then CircuitBreakerOpen (currentTime + (resetTimeoutSecs options)) else CircuitBreakerClosed (errorCount + 1), ())
+                                        other                             -> (other, ())
+                                      
+      failingCall                 = fail $ failureDetails options
+      callIfOpen request ref      = do
+                                      currentTime <- liftIO $ getCurrentTime
+                                      canaryRequest <- liftIO $ atomicModifyIORef' ref $ \status -> case status of 
+                                                              (CircuitBreakerClosed _)  -> (status, False)
+                                                              (CircuitBreakerOpen time) -> if currentTime > time then ((CircuitBreakerOpen (currentTime + (resetTimeoutSecs options))), True) else (status, False)
+                                      
+                                      if canaryRequest then callIfClosed request ref else failingCall
+      breakerService ref request  = do
+                                      status <- liftIO $ readIORef ref
+                                      case status of 
+                                        (CircuitBreakerClosed _)  -> callIfClosed request ref
+                                        (CircuitBreakerOpen time) -> callIfOpen request ref
+                                      
+  in do
+        ref <- liftIO $ newIORef $ CircuitBreakerClosed 0
+        return (ref, breakerService ref)
