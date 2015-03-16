a7d87b26fc3aed0695a0a08c27b358ce1175d57d
Sean Parsons <github@futurenotfound.com>
2015-03-16 08:49:33 +0000

    Added retryingService.

--- src/ServiceFabric/Retry.hs
+++ src/ServiceFabric/Retry.hs
@@ -4,6 +4,7 @@
 
 module ServiceFabric.Retry where
 
+import Control.Exception.Base(SomeException)
 import Data.Typeable
 import ServiceFabric.Types
 import Control.Concurrent
@@ -11,7 +12,6 @@ import Control.Monad.IO.Class
 import Control.Monad.CatchIO
 
 data RetryOptions a = RetryOptions {
-  retryDescription        :: String,
   retryAllowed            :: a -> Bool,
   retryInitialWaitTimeMs  :: Int,
   maximumRetries          :: Int,
@@ -20,8 +20,7 @@ data RetryOptions a = RetryOptions {
 
 defaultRetryOptions :: RetryOptions a
 defaultRetryOptions = RetryOptions { 
-    retryDescription        = "Exceeded maximum number of retries." 
-  , retryAllowed            = (\_ -> True)
+    retryAllowed            = (\_ -> True)
   , retryInitialWaitTimeMs  = 0
   , maximumRetries          = 3
   , retryWaitTimeMultiplier = 0
@@ -30,3 +29,11 @@ defaultRetryOptions = RetryOptions {
 data RetryException = RetryException String deriving (Eq, Show, Typeable)
 instance Exception RetryException
 
+retryingService :: (MonadCatchIO m) => RetryOptions a -> BasicService m a b -> BasicService m a b
+retryingService options service =
+  let attempt attemptCount request  = if (retryAllowed options) request && maxAttempts <= attemptCount
+                                      then catch (service request) (\(_ :: SomeException) -> (wait attemptCount) >> (attempt (attemptCount + 1) request))
+                                      else service request
+      maxAttempts                   = maximumRetries options
+      wait attemptCount             = liftIO $ threadDelay $ round $ fromIntegral (retryInitialWaitTimeMs options) * ((retryWaitTimeMultiplier options) ^ attemptCount)
+  in  attempt 1
