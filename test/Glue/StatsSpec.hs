3624236b8eb38eb07bca6879b571d58dd256b534
Sean Parsons <github@futurenotfound.com>
2015-03-26 08:47:46 +0000

    StatsSpec now fully tests the methods.

--- test/Glue/StatsSpec.hs
+++ test/Glue/StatsSpec.hs
@@ -2,6 +2,7 @@
 
 module Glue.StatsSpec where
 
+import Data.Int
 import Data.Typeable
 import Glue.Stats
 import Glue.Types
@@ -12,23 +13,34 @@ import Test.QuickCheck
 import Control.Exception.Base hiding (throw, throwIO)
 import Control.Exception.Lifted
 import System.Metrics
-
-{-
-recordDistribution
-  , recordAttempts
-  , recordSuccesses
-  , recordFailures
-  , recordLastRequest
-  , recordLastResult
--}
+import qualified System.Metrics.Distribution as MD
+import qualified Data.HashMap.Strict as M
 
 data StatsTestException = StatsTestException deriving (Eq, Show, Typeable)
 instance Exception StatsTestException
 
+data MetricsResult = CounterResult Int64
+                   | GaugeResult Int64
+                   | LabelResult Text
+                   | DistributionResult Int64
+                    deriving (Eq, Show)
+
+checkResult :: Store -> Text -> (MetricsResult -> Expectation) -> Expectation
+checkResult store name check = do
+  allMetrics <- sampleAll store
+  let possibleValue = M.lookup name allMetrics
+  result <- case possibleValue of
+                (Just (Counter counterCount)) -> return $ CounterResult counterCount
+                (Just (Gauge value))          -> return $ GaugeResult value
+                (Just (Label text))           -> return $ LabelResult text
+                (Just (Distribution stats))   -> return $ DistributionResult $ MD.count stats
+                Nothing                       -> fail "No metric."
+  check result
+
 testStats :: String -> 
             (Store -> Text -> BasicService IO Int Int -> IO (BasicService IO Int Int)) ->
-            (Store -> Expectation) ->
-            (Store -> Expectation) ->
+            (Int -> Int -> MetricsResult -> Expectation) ->
+            (Int -> Int -> MetricsResult -> Expectation) ->
             Spec
 testStats methodName method successCheck failureCheck = 
   describe methodName $ do
@@ -38,20 +50,20 @@ testStats methodName method successCheck failureCheck =
         store <- newStore
         wrappedService <- method store name service :: IO (BasicService IO Int Int)
         (wrappedService request) `shouldReturn` result
-        successCheck store
+        checkResult store name $ successCheck request result
     it "Failing call" $ do
-      property $ \(request :: Int, name :: Text) -> do
+      property $ \(request :: Int, result :: Int, name :: Text) -> do
         let service _ = throwIO StatsTestException :: IO Int
         store <- newStore
         wrappedService <- method store name service :: IO (BasicService IO Int Int)
         (wrappedService request) `shouldThrow` (== StatsTestException)
-        failureCheck store
+        checkResult store name $ failureCheck request result
 
 spec :: Spec
-spec = do
-  testStats "recordDistribution" recordDistribution (\_ -> pending) (\_ -> pending)
-  testStats "recordAttempts" recordAttempts (\_ -> pending) (\_ -> pending)
-  testStats "recordSuccesses" recordSuccesses (\_ -> pending) (\_ -> pending)
-  testStats "recordFailures" recordFailures (\_ -> pending) (\_ -> pending)
-  testStats "recordLastRequest" recordLastRequest (\_ -> pending) (\_ -> pending)
-  testStats "recordLastResult" recordLastResult (\_ -> pending) (\_ -> pending)
+spec = parallel $ do
+  testStats "recordDistribution" recordDistribution (\_ -> \_ -> \m -> m `shouldBe` (DistributionResult 1)) (\_ -> \_ -> \m -> m `shouldBe` (DistributionResult 1))
+  testStats "recordAttempts" recordAttempts (\_ -> \_ -> \m -> m `shouldBe` (CounterResult 1)) (\_ -> \_ -> \m -> m `shouldBe` (CounterResult 1))
+  testStats "recordSuccesses" recordSuccesses (\_ -> \_ -> \m -> m `shouldBe` (CounterResult 1)) (\_ -> \_ -> \m -> m `shouldBe` (CounterResult 0))
+  testStats "recordFailures" recordFailures (\_ -> \_ -> \m -> m `shouldBe` (CounterResult 0)) (\_ -> \_ -> \m -> m `shouldBe` (CounterResult 1))
+  testStats "recordLastRequest" recordLastRequest (\r -> \_ -> \m -> m `shouldBe` (LabelResult $ pack $ show r)) (\r -> \_ -> \m -> m `shouldBe` (LabelResult $ pack $ show r))
+  testStats "recordLastResult" recordLastResult (\_ -> \r -> \m -> m `shouldBe` (LabelResult $ pack $ show r)) (\_ -> \_ -> \m -> m `shouldBe` (LabelResult ""))
