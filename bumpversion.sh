4f0b73fdee2ae41009067871832b5189a50af1a9
Sean Parsons <git@futurenotfound.com>
2019-01-17 23:11:10 +0000

    Updated for GHC 8.4.

--- bumpversion.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-bumper -$1 glue-common
-bumper -$1 glue-core
-bumper -$1 glue-ekg
-bumper -$1 glue-example
--- uploadrelease.sh
+++ uploadrelease.sh
@@ -1,5 +1,21 @@
-#!/bin/sh
-stack upload --no-signature glue-common
-stack upload --no-signature glue-core
-stack upload --no-signature glue-ekg
-stack upload --no-signature glue-example
+#!/usr/bin/env bash
+
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $ROOT/glue-core
+cabal sdist
+cabal upload --publish dist/glue-core-*.tar.gz
+
+cd $ROOT/glue-common
+cabal sdist
+cabal upload --publish dist/glue-common-*.tar.gz
+
+cd $ROOT/glue-ekg
+cabal sdist
+cabal upload --publish dist/glue-ekg-*.tar.gz
+
+cd $ROOT/glue-example
+cabal sdist
+cabal upload --publish dist/glue-example-*.tar.gz
+
+echo $ROOT
