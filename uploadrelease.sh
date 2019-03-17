3a6ed98a6a3bb9e9346989c2075cc3c22350b97d
Sean Parsons <git@futurenotfound.com>
2019-03-17 22:48:27 +0000

    uploadrelease.sh script now removes old tarballs.

--- uploadrelease.sh
+++ uploadrelease.sh
@@ -3,18 +3,22 @@
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $ROOT/glue-core
+rm dist/glue-core-*.tar.gz
 cabal sdist
 cabal upload --publish dist/glue-core-*.tar.gz
 
 cd $ROOT/glue-common
+rm dist/glue-common-*.tar.gz
 cabal sdist
 cabal upload --publish dist/glue-common-*.tar.gz
 
 cd $ROOT/glue-ekg
+rm dist/glue-ekg-*.tar.gz
 cabal sdist
 cabal upload --publish dist/glue-ekg-*.tar.gz
 
 cd $ROOT/glue-example
+rm dist/glue-example-*.tar.gz
 cabal sdist
 cabal upload --publish dist/glue-example-*.tar.gz
 
